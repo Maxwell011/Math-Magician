0266d43a19f1b32797a3e6b2f040e2b801d8d67c
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-10-25 16:59:18 +0100

    Created Components and state function

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-quotes */
+import { Component } from 'react';
+import Calculator from './Components/calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className='calculator-container'>
+        <Calculator />
+      </div>
+    );
+  }
 }
-
 export default App;
