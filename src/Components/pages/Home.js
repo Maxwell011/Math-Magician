8f4c68fcbe5512db5411100af513ac7e4f0ba4ad
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-11-01 22:11:55 +0100

    Added Route for the page

---
+++ src/Components/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <main>
+      <header>
+        <h2 className="main-heading">Welcome To Our Page</h2>
+      </header>
+
+      <p>
+        Mathematics is one of the most important subjects. Mathematics is a
+        subject of numbers, shapes, data, measurements and also logical
+        activities. It has a huge scope in every field of our life, such as
+        medicine, engineering, finance, natural science, economics, etc.
+      </p>
+      <br />
+      <p>
+        We are all surrounded by a mathematical world. The concepts, theories
+        and formulas that we learn in Maths books have huge applications in
+        real-life. To find the solutions for various problems we need to learn
+        the formulas and concepts. Therefore, it is important to learn this
+        subject to understand its various applications and significance.
+      </p>
+    </main>
+  </div>
+);
+
+export default Home;
