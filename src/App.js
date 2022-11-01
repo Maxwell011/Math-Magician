b3c49092f7290c2500ceddf23075c668b12d729b
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-11-01 22:09:18 +0100

    Added route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './Components/calculator';
+import Home from './Components/pages/Home';
+import Quote from './Components/pages/Quote';
+import NavBar from './Components/pages/NavBar';
 
 const App = () => (
-  <div className="calculator-container">
-    <Calculator />
-  </div>
+  <Router>
+    <NavBar />
+    <div className="calculator-container">
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/calculator" exact element={<Calculator />} />
+        <Route path="/quote" exact element={<Quote />} />
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
